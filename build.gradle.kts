7c448adb6cd72fce0691debd6b2a9fca4e8f2945
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2025-06-05 17:20:29 +1000

    del packege

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,23 +26,3 @@ kotlin {
     jvmToolchain(17)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("gpr") {
-            from(components["java"])
-            groupId = "net.Mirik9724"
-            artifactId = "MirikAPI"
-            version = version
-        }
-    }
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Mirik9724/MirikAPI")
-            credentials {
-                username = System.getenv("GITHUB_USERNAME") ?: "Mirik9724"
-                password = System.getenv("GITHUB_TOKEN") ?: file("github_token.txt").readLines().first().trim()
-            }
-        }
-    }
-}
